c36eacef7008755e6c5fab64f83777849f77e2c8
okanbisgin <okan.bisgin@yazara.com>
2025-02-17 18:07:58 +0900

    Initial commit - Added Binary String Analysis function

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a binary string:");
+        string binaryString = Console.ReadLine();
+
+        if (IsGoodBinaryString(binaryString))
+        {
+            Console.WriteLine("The binary string is good.");
+        }
+        else
+        {
+            Console.WriteLine("The binary string is not good.");
+        }
+    }
+
+    static bool IsGoodBinaryString(string binaryString)
+    {
+        int count0 = 0;
+        int count1 = 0;
+
+        foreach (char c in binaryString)
+        {
+            if (c == '0') count0++;
+            else if (c == '1') count1++;
+        }
+
+        if (count0 != count1) return false;
+
+        count0 = 0;
+        count1 = 0;
+        foreach (char c in binaryString)
+        {
+            if (c == '0') count0++;
+            else if (c == '1') count1++;
+
+            if (count1 < count0) return false;
+        }
+
+        return true;
+    }
+}
